6044884dc3a38966c31c87f10e470091a10e0960
Cathulhu17 <fontanella.luciano8@gmail.com>
2025-10-30 22:35:14 +0000

    html file

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
-let express = require('express');
-let app = express();
+var express = require('express');
+var app = express();
 
-app.get("/",function(req,res){
-    res.send(__dirname + "/views/index.html");
+// Sirve el archivo HTML ubicado en /views/index.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-module.exports= app;
+// No modifiques esta línea
+module.exports = app;
 
 
 
