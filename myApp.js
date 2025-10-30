3de2b9c8a27e3cea74349188f95f05d8fd489cfc
Cathulhu17 <fontanella.luciano8@gmail.com>
2025-10-30 22:45:02 +0000

    server static assets

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// Servir archivos estáticos desde la carpeta /public
+app.use("/public",express.static(__dirname + "/public"));
+
 // Sirve el archivo HTML ubicado en /views/index.html
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
