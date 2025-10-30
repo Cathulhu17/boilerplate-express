d121189ccd4d75323e487ff7ef0941aa0239705c
Cathulhu17 <fontanella.luciano8@gmail.com>
2025-10-30 23:47:05 +0000

    time user

--- myApp.js
+++ myApp.js
@@ -26,9 +26,8 @@ app.get("/json", function(req, res) {
   res.json({ "message": message });
 });
 
-//ruta hora
-app.get("/now", function(req, res, next){
-    req.time = new Date().toString();
+app.get("/now", function(req, res, next) {
+  req.time = new Date().toString();
   next();
 }, function(req, res) {
   res.json({ time: req.time });
