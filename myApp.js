395131edc1576fa209b4cb482233000cb06774bf
Cathulhu17 <fontanella.luciano8@gmail.com>
2025-10-30 23:34:10 +0000

    middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 var express = require('express');
 var app = express();
 
+// ✅ Root-level middleware para loguear todas las solicitudes
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 // Servir archivos estáticos desde la carpeta /public
 app.use("/public",express.static(__dirname + "/public"));
 
@@ -18,6 +24,19 @@ app.get("/json", function(req, res) {
     message = message.toUpperCase();
   }
   res.json({ "message": message });
+});
+
+// Ruta GET /name
+app.get("/name", function(req, res) {
+  const { first, last } = req.query;
+  res.json({ name: `${first} ${last}` });
+});
+
+// Ruta POST /name
+app.post("/name", function(req, res) {
+  const firstName = req.body.first;
+  const lastName = req.body.last;
+  res.json({ name: `${firstName} ${lastName}` });
 });
 
 
